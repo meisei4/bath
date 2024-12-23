2ab9517b584660d04d7ad37e079ecdd34bc9f916
djneville <30916597+djneville@users.noreply.github.com>
2024-12-22 19:38:25 -0600

    rigged skele
    rigged

--- godot/Test/Test.gd
+++ godot/Test/Test.gd
@@ -10,17 +10,17 @@ var scale_factor: int = 2  # Scale factor (e.g., 2 for 2x scaling) just to see t
 
 
 func _ready() -> void:
-    get_window().size = Vector2i(width * scale_factor, height * scale_factor)
-    center_viewport()
+	get_window().size = Vector2i(width * scale_factor, height * scale_factor)
+	center_viewport()
 
 
 func center_viewport() -> void:
-    camera = Camera2D.new()  #TODO: COULD JUST ATTACH THE SNOW AND STUFF TO CAMERA
-    camera.zoom = Vector2(1.0, 1.0)
-    add_child(camera)
+	camera = Camera2D.new()  #TODO: COULD JUST ATTACH THE SNOW AND STUFF TO CAMERA
+	camera.zoom = Vector2(1.0, 1.0)
+	add_child(camera)
 
 
 func _process(_delta: float) -> void:
-    for child: Node in get_children():
-        if child is Iruka:
-            camera.position = child.position
+	for child: Node in get_children():
+		if child is Iruka:
+			camera.position = child.position
