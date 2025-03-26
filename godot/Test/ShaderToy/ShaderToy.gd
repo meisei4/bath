83838a52341d1c1a3e62916749fe58529dbb574b
pl <lanur622@gmail.com>
2025-03-26 22:32:13 +0900

    figure out the clearing stuff in the rendering pipeline, it must be whats not allowing for a level texture

--- godot/Test/ShaderToy/ShaderToy.gd
+++ godot/Test/ShaderToy/ShaderToy.gd
@@ -1,14 +1,14 @@
 extends Node2D
 class_name ShaderToy
 
+var caustic_shader: Shader = load("res://Resources/Shaders/water.gdshader")
+var caustic_material: ShaderMaterial
 #var feedback_buffer_shader: Shader = load("res://Resources/Shaders/simple_feedback_buffer.gdshader")
 #TODO:^^the above works fine just with up till BufferC being the texture fed into the shader as feedback
 #TODO: see https://www.shadertoy.com/view/W3s3W8, it works the same way i think
-var ripple_shader: Shader = load("res://Resources/Shaders/finite_approx_ripple.gdshader")
+#var ripple_shader: Shader = load("res://Resources/Shaders/finite_approx_ripple.gdshader")
+var ripple_shader: Shader = load("res://Resources/Shaders/all_in_one_ripple.gdshader")
 var ripple_material: ShaderMaterial
-
-var caustic_shader: Shader = load("res://Resources/Shaders/water.gdshader")
-var caustic_material: ShaderMaterial
 
 var background_image: Texture = load("res://Assets/Textures/rocks.jpg")
 
@@ -30,21 +30,23 @@ var MainImage: TextureRect
 func _ready() -> void:
     var main_viewport_size: Vector2 = get_viewport_rect().size  # THIS WILL ALWAYS BE THE WINDOW I THINK
     BufferA = SubViewport.new()
-    BufferA.transparent_bg = true
+    BufferA.render_target_clear_mode = SubViewport.CLEAR_MODE_ONCE
     BufferA.size = main_viewport_size
 
     BufferA_Image = ColorRect.new()
+    BufferA_Image.color = Color(0.0, 0.0, 0.0, 0.0)
     BufferA_Image.size = main_viewport_size
     ripple_material = ShaderMaterial.new()
     ripple_material.shader = self.ripple_shader
     BufferA_Image.material = self.ripple_material
 
     BufferB = SubViewport.new()
-    BufferB.transparent_bg = true
+    BufferB.render_target_clear_mode = SubViewport.CLEAR_MODE_ONCE
     BufferB.size = main_viewport_size
 
     BufferB_Image = TextureRect.new()
     BufferB_Image.size = main_viewport_size
+
     BufferB_Image.texture = BufferA.get_texture()
 
     BufferC = TextureRect.new()
@@ -58,44 +60,45 @@ func _ready() -> void:
     add_child(BufferC)
 
     ripple_material.set_shader_parameter("iChannel0", BufferC.texture)
+    ripple_material.set_shader_parameter("iChannel1", background_image)
     ripple_material.set_shader_parameter("iResolution", main_viewport_size)
 
-    BufferD = SubViewport.new()
-    BufferD.transparent_bg = true
-    BufferD.size = main_viewport_size
+    #BufferD = SubViewport.new()
+    #BufferD.render_target_clear_mode = SubViewport.CLEAR_MODE_ONCE
+    #BufferD.size = main_viewport_size
+#
+    #BufferD_Image = ColorRect.new()
+    #BufferD_Image.size = main_viewport_size
+    #caustic_material = ShaderMaterial.new()
+    #caustic_material.shader = self.caustic_shader
+    #BufferD_Image.material = self.caustic_material
+#
+    #BufferE = SubViewport.new()
+    #BufferE.render_target_clear_mode = SubViewport.CLEAR_MODE_ONCE
+    #BufferE.size = main_viewport_size
+#
+    #BufferE_Image = TextureRect.new()
+    #BufferE_Image.size = main_viewport_size
+    #BufferE_Image.texture = BufferD.get_texture()
+#
+    #MainImage = TextureRect.new()
+    #MainImage.size = main_viewport_size
+    #MainImage.texture = BufferE.get_texture()
 
-    BufferD_Image = ColorRect.new()
-    BufferD_Image.size = main_viewport_size
-    caustic_material = ShaderMaterial.new()
-    caustic_material.shader = self.caustic_shader
-    BufferD_Image.material = self.caustic_material
+    #BufferD.add_child(BufferD_Image)
+    #BufferE.add_child(BufferE_Image)
+    #add_child(BufferD)
+    #add_child(BufferE)
+    #add_child(MainImage)
 
-    BufferE = SubViewport.new()
-    BufferE.transparent_bg = true
-    BufferE.size = main_viewport_size
-
-    BufferE_Image = TextureRect.new()
-    BufferE_Image.size = main_viewport_size
-    BufferE_Image.texture = BufferD.get_texture()
-
-    MainImage = TextureRect.new()
-    MainImage.size = main_viewport_size
-    MainImage.texture = BufferE.get_texture()
-
-    BufferD.add_child(BufferD_Image)
-    BufferE.add_child(BufferE_Image)
-    add_child(BufferD)
-    add_child(BufferE)
-    add_child(MainImage)
-
-    caustic_material.set_shader_parameter("iChannel1", background_image)
-    caustic_material.set_shader_parameter("iChannel3", MainImage.texture)
+    #caustic_material.set_shader_parameter("iChannel1", background_image)
+    #caustic_material.set_shader_parameter("iChannel3", MainImage.texture)
 
 
 #TODO: I have no idea about the frozen shakey stuff in the result, seems like its not able to recognize even the following:
 # 1- there needs to be an understood equilibrium of the heightmap
 # 2- might be screwing up the viewport stuff up above
-func _process(delta: float) -> void:
+func _physics_process(delta: float) -> void:
     iTime += delta
     var mouse_coords: Vector2 = get_viewport().get_mouse_position()
     var mouse_z: int = 1.0 if Input.is_mouse_button_pressed(MOUSE_BUTTON_LEFT) else 0.0
@@ -103,6 +106,3 @@ func _process(delta: float) -> void:
     #print("Mouse Position: ", mouse_coords, " | Mouse Z (pressed): ", mouse_z)
     ripple_material.set_shader_parameter("iTime", iTime)
     ripple_material.set_shader_parameter("iMouse", iMouse)
-
-    caustic_material.set_shader_parameter("iTime", iTime)
-    caustic_material.set_shader_parameter("iMouse", iMouse)
