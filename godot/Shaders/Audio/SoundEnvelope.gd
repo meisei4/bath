00761747459d0daa363b1c219f0b3f7bb8fc1fc5
iann <lanur622@gmail.com>
2025-05-14 13:53:25 +0900

    re-organize to prepare for music dimensional analysis 勉強

--- godot/Shaders/Audio/SoundEnvelope.gd
+++ godot/Shaders/Audio/SoundEnvelope.gd
@@ -2,13 +2,13 @@ extends Node2D
 class_name SoundEnvelope
 
 var BufferAShaderNode: ColorRect
-var BufferAShader: Shader = load("res://Resources/Shaders/Audio/BufferA_sound_envelope.gdshader")
-#var BufferAShader: Shader = load("res://Resources/Shaders/Audio/optimized_envelope_buffer_a.gdshader")
+var BufferAShader: Shader = load("res://Resources/Shaders/Audio/SoundEnvelopeWIP/BufferA_sound_envelope.gdshader")
+#var BufferAShader: Shader = load("res://Resources/Shaders/Audio/SoundEnvelopeWIP/optimized_envelope_buffer_a.gdshader")
 var BufferAShaderMaterial: ShaderMaterial
 
 var BufferBShaderNode: ColorRect
-var BufferBShader: Shader = load("res://Resources/Shaders/Audio/Image_sound_envelope.gdshader")
-#var BufferBShader: Shader = load("res://Resources/Shaders/Audio/optimized_envelope_buffer_b.gdshader")
+var BufferBShader: Shader = load("res://Resources/Shaders/Audio/SoundEnvelopeWIP/Image_sound_envelope.gdshader")
+#var BufferBShader: Shader = load("res://Resources/Shaders/Audio/SoundEnvelopeWIP/optimized_envelope_buffer_b.gdshader")
 var BufferBShaderMaterial: ShaderMaterial
 
 var waveform_texture: WaveformTexture
--- godot/Shaders/Audio/WaveformVisualizer.gd
+++ godot/Shaders/Audio/WaveformVisualizer.gd
@@ -1,14 +1,12 @@
 extends Node2D
-class_name AudioTextureVisualizer
+class_name WaveformVisualizer
 
 var BufferAShaderNode: ColorRect
-#var BufferAShader: Shader = load("res://Resources/Shaders/Audio/basic_waveform.gdshader")
-var BufferAShader: Shader = load(
-    "res://Resources/Shaders/Audio/basic_fast_fourier_transform_spectrum.gdshader"
-)
+var BufferAShader: Shader = load("res://Resources/Shaders/Audio/waveform.gdshader")
+
 var BufferAShaderMaterial: ShaderMaterial
 
-var shadertoy_audio_texture: ShaderToyAudioTexture
+var audio_texture: WaveformTexture
 
 var BufferA: SubViewport
 var MainImage: TextureRect
@@ -21,7 +19,7 @@ func _ready() -> void:
     var res: Vector2i = Vector2i(855, 480)
     DisplayServer.window_set_size(res)  #TODO: this doesnt do what you think it does
     iResolution = res
-    BufferA = create_buffer_viewport(iResolution)
+    BufferA = ShaderToyUtil.create_buffer_viewport(iResolution)
     BufferAShaderMaterial = ShaderMaterial.new()
     BufferAShaderNode = ColorRect.new()
     BufferAShaderNode.size = iResolution
@@ -32,31 +30,18 @@ func _ready() -> void:
     MainImage = TextureRect.new()
     MainImage.texture = BufferA.get_texture()
     MainImage.flip_v = true
-    var music_resource: AudioStream = load(AudioConsts.SHADERTOY_MUSIC_TRACK_EXPERIMENT)
+    var music_resource: AudioStream = load(AudioConsts.HELLION_MP3)
     #var music_resource: AudioStream = load(AudioConsts.HELLION)
     AudioManager.play_music(music_resource)
     #TODO: ^^^ ew, figure out how to perhaps make it more obvious that the audio texture can target whatever audio bus...
-    shadertoy_audio_texture = ShaderToyAudioTexture.new()  #TODO: this has to target a specific audio bus internally, figure out a better way
+    audio_texture = WaveformTexture.new()  #TODO: this has to target a specific audio bus internally, figure out a better way
 
     BufferA.add_child(BufferAShaderNode)
     add_child(BufferA)
     add_child(MainImage)
-    add_child(shadertoy_audio_texture)
-
-
-func create_buffer_viewport(resolution: Vector2) -> SubViewport:
-    var subviewport: SubViewport = SubViewport.new()
-    subviewport.size = resolution
-    subviewport.disable_3d = true
-    #TODO: be able to sample 16 bit at least??? even though i try to set the image format to FORMAT_RF (32-bit floats when i draw to the audio texture
-    subviewport.use_hdr_2d = true
-    RenderingServer.set_default_clear_color(Color.BLACK)
-    subviewport.render_target_clear_mode = SubViewport.CLEAR_MODE_ALWAYS
-    subviewport.render_target_update_mode = SubViewport.UPDATE_ALWAYS
-    return subviewport
-
+    add_child(audio_texture)
 
 #TODO: its very important to control frame rate with these audio shaders
 func _process(_delta: float) -> void:
-    iChannel0 = shadertoy_audio_texture.audio_texture
+    iChannel0 = audio_texture.audio_texture
     BufferAShaderMaterial.set_shader_parameter("iChannel0", iChannel0)
