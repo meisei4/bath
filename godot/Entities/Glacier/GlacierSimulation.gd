a382aa27fde72b72ae768ab75cc921f8ad3d192f
iann <lanur622@gmail.com>
2025-05-28 10:11:36 +0900

    alpha stuff in opengl i have no idea what is happening

--- godot/Entities/Glacier/GlacierSimulation.gd
+++ godot/Entities/Glacier/GlacierSimulation.gd
@@ -26,9 +26,9 @@ func _ready() -> void:
         _on_iceberg_manager_force_fracture_glacier_cell
     )
     update_dirty_tiles()
-    var music_res: AudioStream = load(AudioConsts.MUSIC_TRACK_1) as AudioStream
-    AudioPoolManager.play_music(music_res, 1.0)
-    AudioEffectManager.add_reverb(AudioBus.BUS.MUSIC)
+    #var music_res: AudioStream = load(AudioConsts.MUSIC_TRACK_1) as AudioStream
+    #AudioPoolManager.play_music(music_res, 1.0)
+    #AudioEffectManager.add_reverb(AudioBus.BUS.MUSIC)
     #AudioEffectManager.add_reverb(AudioBus.BUS.SFX)
 
 
@@ -54,8 +54,8 @@ func _on_simulation_tick() -> void:
 
 func _on_iceberg_manager_force_fracture_glacier_cell(cell_position: Vector2i) -> void:
     hydrofracture_manager.fracture_glacier_cell(glacier_data, cell_position)
-    var sfx_res: AudioStream = load(AudioConsts.SFX_544_METAL_ICE_SHARD)
-    AudioPoolManager.play_sfx(sfx_res, -15.0)
+    #var sfx_res: AudioStream = load(AudioConsts.SFX_544_METAL_ICE_SHARD)
+    #AudioPoolManager.play_sfx(sfx_res, -15.0)
     update_dirty_tiles()
 
 
@@ -80,8 +80,8 @@ func _on_iceberg_cluster_formed(cluster_id: int, iceberg_cluster: Array[Vector2i
     var end_index: int = water_shader.iceberg_tile_positions.size()
     water_shader.cluster_offsets.append(start_index)
     water_shader.cluster_offsets.append(end_index)
-    var sfx_res: AudioStream = load(AudioConsts.SFX_469_SPLASH)
-    AudioPoolManager.play_sfx(sfx_res, -3.0)
+    #var sfx_res: AudioStream = load(AudioConsts.SFX_469_SPLASH)
+    #AudioPoolManager.play_sfx(sfx_res, -3.0)
     update_dirty_tiles()
 
 
