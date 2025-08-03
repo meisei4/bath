fabb81e606a71e3c3aaff3921166a35c23b68d40
iann <lanur622@gmail.com>
2025-08-04 01:50:17 +0900

    more time wasted

--- rust/examples/ghost_dither_opengl11_final.rs
+++ rust/examples/ghost_dither_opengl11_final.rs
@@ -7,15 +7,14 @@ use raylib::color::Color;
 use raylib::consts::CameraProjection;
 
 use bath::fixed_func::silhouette::{
-    build_radial_magnitudes, deform_mesh_by_silhouette_radii, generate_silhouette_image, ANGULAR_VELOCITY, MODEL_POS,
-    MODEL_SCALE,
+    bake_samples_for_single_mesh_no_rotation, blend_into_mesh, ANGULAR_VELOCITY, MODEL_POS, MODEL_SCALE,
 };
 use raylib::drawing::{RaylibDraw, RaylibDraw3D, RaylibMode3DExt};
 use raylib::math::Vector3;
-use raylib::models::{RaylibMesh, RaylibModel};
+use raylib::models::RaylibModel;
 
 const DT: f32 = 0.1;
-const NUM_SAMPLES: usize = 80;
+const NUM_SAMPLES: usize = 40;
 fn main() {
     let mut i_time = 0.0f32;
     let mut mesh_rotation = 0.0f32;
@@ -29,41 +28,13 @@ fn main() {
     };
     let screen_w = render.handle.get_screen_width();
     let screen_h = render.handle.get_screen_height();
+    let sample_vertices = bake_samples_for_single_mesh_no_rotation(DT, NUM_SAMPLES, screen_w, screen_h, &mut render);
     let mut model = render.handle.load_model(&render.thread, SPHERE_PATH).unwrap();
-    let silhouette_img = generate_silhouette_image(screen_w, screen_h, i_time);
-    let silhouette_radii = build_radial_magnitudes(&silhouette_img);
-    deform_mesh_by_silhouette_radii(&mut model.meshes_mut()[0], &silhouette_radii);
-    let mut sample_vertices = Vec::with_capacity(NUM_SAMPLES);
-    for i in 0..NUM_SAMPLES {
-        let sample_time = i as f32 * DT;
-        let sample_yaw = -sample_time * ANGULAR_VELOCITY;
-        let silhouette_img = generate_silhouette_image(screen_w, screen_h, sample_time);
-        let silhouette_radii = build_radial_magnitudes(&silhouette_img);
-        let mut model_i = render.handle.load_model(&render.thread, SPHERE_PATH).unwrap();
-        {
-            let vertices = model_i.meshes_mut()[0].vertices_mut();
-            for vertex in vertices.iter_mut() {
-                let x0 = vertex.x;
-                let z0 = vertex.z;
-                vertex.x = sample_yaw.cos() * x0 + sample_yaw.sin() * z0;
-                vertex.z = -sample_yaw.sin() * x0 + sample_yaw.cos() * z0;
-            }
-        }
-        deform_mesh_by_silhouette_radii(&mut model_i.meshes_mut()[0], &silhouette_radii);
-        let sample_vertices_i: Vec<Vector3> = model_i.meshes()[0].vertices().iter().cloned().collect();
-        sample_vertices.push(sample_vertices_i);
-    }
-
     while !render.handle.window_should_close() {
         i_time += render.handle.get_frame_time();
-        // mesh_rotation += ANGULAR_VELOCITY * render.handle.get_frame_time();
-        mesh_rotation = 0.0;
-        let sample_idx = ((i_time / DT).floor() as usize) % NUM_SAMPLES;
-        let sample_vertices_src = &sample_vertices[sample_idx];
-        let sample_vertices_dst = model.meshes_mut()[0].vertices_mut();
-        for (dst, src) in sample_vertices_dst.iter_mut().zip(sample_vertices_src.iter()) {
-            *dst = *src;
-        }
+        mesh_rotation -= ANGULAR_VELOCITY * render.handle.get_frame_time();
+        // mesh_rotation = 0.0;
+        blend_into_mesh(&mut model.meshes_mut()[0], &sample_vertices, i_time, DT);
         let mut draw_handle = render.handle.begin_drawing(&render.thread);
         draw_handle.clear_background(Color::BLACK);
         let mut rl3d = draw_handle.begin_mode3D(observer);
